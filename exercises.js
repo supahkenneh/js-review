2b45c74bdd0de9ad1c1315ef83504d5f71038b27
Kenny Chung <kcmac@kennys-mbp.gateway.ht.net>
2018-04-15 12:28:35 -1000

    up to 16

--- exercises.js
+++ exercises.js
@@ -43,22 +43,41 @@ Console log the variable
 Console log the value at index 1.
 Console log the value at index 3.*/
 
+var japanPrefectures = ['Tokyo', 'Kyoto', 'Osaka', 'Sapporo', 'Yokohama'];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
+
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
 Console log the variable
 Console log "Earth" using the index position
 Console log "Uranus" using the index position*/
 
+var planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+console.log(planets);
+console.log(planets[2]);
+console.log(planets[6]);
+
 /*7. Variables with Array for its value.
 Declare a variable named india and assign the value of 3 popular tourist attractions.  
 Console log the variable.*/
+
+var india = ['Taj Mahal', 'Ganges River', 'Mahabodhi Temple'];
+console.log(india);
 
 /*8. Adding new elements to an array.
 Add another popular tourist attraction to the india variable from position 1 of the index.
 (hint, it is not the push() method). Console log the variable.*/
 
+india.unshift('Golden Temple');
+console.log(india);
+
 /*9. Removing the last element from an array.
 Using the same india variable, remove the last element from the array. Console log the variable. */
+
+india.pop();
+console.log(india);
 
 /*10. Removing an element from an array into a new array. 
 Removethe first two elements in the snacks array and return a new array.
@@ -66,11 +85,19 @@ Console log the new variable. */
 
 var snacks = ["peanut butter cup oreo", "birthday cake oreo", "banana split oreo", "watermelon oreo", "cookie dough oreo", "creamsicle oreo"];
 
+var betterSnacks = snacks.slice(2, 6);
+console.log(betterSnacks);
+
 /*11. Declare two variables with names of your choosing and use the triple comparison operator (===) to compare:
 a) two strings "Racecar" and "racecar"
 b) a string and a number "three" and 3
 
 console log each variable*/
+
+var comparison = "Racecar" === 'racecar';
+var secondComparison = 'three' === 3;
+console.log(comparison);
+console.log(secondComparison);
 
 /*12.  Declare a literal object named "hotel" with properties and values set to:
 
@@ -81,6 +108,15 @@ console log each variable*/
 
  console log the object*/
 
+var hotel = {
+    name: 'Hilton',
+    rooms: 200,
+    pool: true,
+    checkIn: null,
+}
+
+console.log(hotel);
+
 /*13.  Declare a literal object named "car" with properties and values set to:
 
     make  => "BMW"
@@ -90,6 +126,15 @@ console log each variable*/
 
  console log the object*/
 
+ var car = {
+     make: 'BMW',
+     model: 'X5',
+     year: 2015,
+     doors: 4,
+ }
+
+ console.log(car);
+
 /*14.  Declare a literal object named bigBox and set its value to be an empty object.
 
 Add a property named size and set it to a random number value between 0 and 50 (hint, use Math.something...)
@@ -97,6 +142,13 @@ Add a property named color and set it to any color.
 Add a property named contents and set it to be an empty array
 
 console log the object*/
+
+var bigBox = {};
+bigBox.size = Math.floor(Math.random() * 50);
+bigBox.color = 'blue';
+bigBox.contents = [];
+
+console.log(bigBox);
 
 /*15.  Declare a literal object named "colorMeBadd" with properties and values set to:
 
@@ -113,9 +165,40 @@ console log the object*/
 
 console log the object */ 
 
-/*16. As the general manager for the DevLeague Prep Beer Pong Team, your job is to select the starting players from the roster array. Create two variables, Ateam and Bteam and assign each an empty array. You will then assigned players that have an even number index position to the Ateam and odd number index position to the Bteam. Let the games begin! Console log the results.*/
+var colorMeBadd = {
+    akai: 'red',
+    aoi: 'blue',
+    midori: 'green',
+    shiroi: 'white',
+    pinku: 'pink',
+    murasaki: 'purple',
+    isJapanese: true,
+}
+
+console.log(colorMeBadd);
+
+/*16. As the general manager for the DevLeague Prep Beer Pong Team, your job is to select the starting players from the roster array. 
+Create two variables, Ateam and Bteam and assign each an empty array. 
+You will then assigned players that have an even number index position to the Ateam and odd number index position to the Bteam. 
+Let the games begin! Console log the results.*/
 
 var roster = ["Alex", "Laura", "Sami", "Jasmine", "Adam", "Dakota", "Jonathon", "Luther", "Kevin", "Vic"];
+var Ateam = [];
+var Bteam = [];
+
+function gameTime (arr){
+    for (var i = 0; i < arr.length; i++){
+        if (i %2 === 0){
+            Ateam.push(arr[i]);
+        }else{
+            Bteam.push(arr[i]);
+        }
+    }
+}
+
+gameTime(roster);
+console.log(Ateam);
+console.log(Bteam);
 
 /*17. Using the japanPrefectures array you created in exercise 5, create a for loop that iterates through this array and console logs the following message:
 
